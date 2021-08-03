54ab94cebebd783b31e4289f07e8caf78c610fd1
Jan Barášek <janbarasek@gmail.com>
2021-08-03 20:34:04 +0200

    Add DIC extension

---
+++ src/ShopPriceExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Shop\Price;
+
+
+use Nette\DI\CompilerExtension;
+
+final class ShopPriceExtension extends CompilerExtension
+{
+	public function beforeCompile(): void
+	{
+		$builder = $this->getContainerBuilder();
+
+		$builder->addDefinition($this->prefix('currencyResolver'))
+			->setFactory(CurrencyResolver::class);
+
+		$builder->addDefinition($this->prefix('priceRenderer'))
+			->setFactory(PriceRenderer::class);
+	}
+}
